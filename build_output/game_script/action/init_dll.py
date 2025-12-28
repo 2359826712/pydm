dc34752ba02c7a90911a0081142ef9a6f5fe2948
2359826712 <2359826712@qq.com>
2025-12-28 12:37:38 +0800

    refactor: 移除未使用的Steam/Global图标处理逻辑并简化初始化流程
    删除不再需要的_handle_steam_glob_click方法和相关的滚轮操作逻辑
    简化update方法，直接调用_async_init_data并设置init_dll标志
    移除冗余的ESC菜单检查和其他界面处理代码

--- build_output/game_script/action/init_dll.py
+++ build_output/game_script/action/init_dll.py
@@ -60,17 +60,6 @@ class Init_Dll(py_trees.behaviour.Behaviour):
         init_thread.start()
         time.sleep(2)
 
-    def _handle_steam_glob_click(self, x1, y1, x2, y2, click_x, click_y_steam, click_y_glob):
-        """处理 Steam/Global 图标识别并点击"""
-        steam_pos = arc_api.FindColorE(x1, y1, x2, y2, "f7ecdd-000000|020202-000000|000000-000000", 1.0, 0)
-        steam_pos = steam_pos.split("|")
-        glob_pos = arc_api.FindColorE(x1, y1, x2, y2, "312f2c-000000|f4e9da-000000|b8b0a5-000000", 1.0, 0)
-        glob_pos = glob_pos.split("|")
-        
-        if int(steam_pos[0]) > 0 or int(glob_pos[0]) > 0:
-            arc_api.mouse_click(click_x, click_y_steam, 0)
-        else:
-            arc_api.mouse_click(click_x, click_y_glob, 0)
 
     def update(self) -> py_trees.common.Status:
         pos = arc_api.FindColorE(26,787,266,869,"54c8e9-000000|ffffff-000000",1.0,0)
@@ -79,20 +68,12 @@ class Init_Dll(py_trees.behaviour.Behaviour):
             print("正在游戏")
             time.sleep(0.5)
             self.blackboard.in_game = True
+            self._async_init_data()
+            self.blackboard.init_dll = True
             time.sleep(1)
             return py_trees.common.Status.RUNNING
         # 2. 检查继续按钮
         if self._check_and_click_continue():
-            return py_trees.common.Status.RUNNING
-        # 4. 检查是否在 ESC 菜单或其他界面
-        pos = arc_api.FindColorE(1317,124,1391,148,"ffbc13-000000|090c19-000000",1.0,0)
-        pos = pos.split("|")
-        if int(pos[0]) <= 0 :
-            time.sleep(0.5)
-            print("点击esc")
-            self.click_account = 0
-            arc_api.click_keyworld("esc")
-            time.sleep(1.5)
             return py_trees.common.Status.RUNNING
         # 5. 检查特定位置颜色 (Near Pos?)
         near_pos = arc_api.FindColorE(685,117,758,152,"f9eedf-000000",1.0,0)
@@ -102,52 +83,6 @@ class Init_Dll(py_trees.behaviour.Behaviour):
             time.sleep(1.5)
             arc_api.mouse_click(722,136,0)
             return py_trees.common.Status.RUNNING
-
-        
-        
-        # 滚轮操作
-        while self.account != 3:
-             # 6. 主要逻辑：点击并检查加号位置
-            time.sleep(1.5)
-            arc_api.mouse_click(830,236,1)
-            time.sleep(1.5)
-            
-            # 检查位置 1
-            add_pos = arc_api.FindColorE(830,236,1038,446,"37373f-000000",1.0,0)
-            add_pos = add_pos.split("|") 
-            if int(add_pos[1]) > 0 : 
-                self._async_init_data()
-                self._handle_steam_glob_click(837, 277, 860, 300, 910, 320, 288)
-                self.blackboard.init_dll = True
-            else:
-                # 没找到位置 1，尝试位置 2
-                time.sleep(1.5)
-                arc_api.mouse_click(1275,236,1)
-            
-            time.sleep(1.5)
-            arc_api.mouse_click(1275,236,1)
-            time.sleep(1.5)
-            
-            # 检查位置 2
-            add_pos = arc_api.FindColorE(1275,236,1486,457,"37373f-000000",1.0,0)
-            add_pos = add_pos.split("|") 
-            if int(add_pos[1]) > 0 : 
-                self._async_init_data()
-                self._handle_steam_glob_click(1283, 278, 1305, 299, 1355, 320, 290)
-                self.blackboard.init_dll = True
-            else:
-                time.sleep(1.5)
-                arc_api.mouse_click(830,236,1)
-                
-            time.sleep(1.5)
-            self.account += 1
-            time.sleep(0.5)
-            
-        # 完成后返回
-        if self.blackboard.init_dll:
-            time.sleep(0.5)
-            print("返回")
-            self.click_account = 0
-            arc_api.click_keyworld("esc")
-        self.account = 0
+        self._async_init_data()
+        self.blackboard.init_dll = True
         return py_trees.common.Status.RUNNING
